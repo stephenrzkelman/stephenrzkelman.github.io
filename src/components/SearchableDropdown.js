f8394f9dec30ce162fad94d5ce097e3793de26e2
Stephen Kelman <stephenrkelman@gmail.com>
2024-01-20 14:36:54 -0800

    basic searchability added

--- src/components/SearchableDropdown.js
+++ src/components/SearchableDropdown.js
@@ -5,20 +5,28 @@ import { useState } from 'react';
 function SearchableDropdown(props){
     // search bar
     // dropdown
+
+    const [filterFunction, setFilterFunction] = useState(allowAll);
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [value, setValue] = useState("");
+
+    function allowAll(pokemon_name){
+        return pokemon_name.toString().toLowerCase().includes(value.toLowerCase());
+    }
+
     return(
         <div className="searchable-dropdown">
             <input 
                 type="text"
                 className="text-input"
                 value={value}
-                onClick={()=>setDropdownOpen(!dropdownOpen)}
+                onClick={()=>{setDropdownOpen(!dropdownOpen); console.log(`value: \"${value}\"`)}}
+                onChange={(e)=> {setValue(e.target.value)}}
             />
             {
                 dropdownOpen &&
                 <DropdownList 
-                    items={props.items.sort()}
+                    items={props.items.sort().filter(allowAll)}
                     setValue={setValue}
                     closeDropdown={setDropdownOpen}
                     selectionAction={props.selectionAction}
