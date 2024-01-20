48f19ada499ac81bb12423444d1c1f8235ae9dad
Stephen Kelman <stephenrkelman@gmail.com>
2024-01-20 14:12:15 -0800

    updated TODOs

--- src/App.js
+++ src/App.js
@@ -10,11 +10,6 @@ function App() {
       <div style={{textAlign:"left"}}>
         <h2>Known Issues/WIP:</h2>
         <ul>
-          <li>Some pokemon in dropdown cause page to crash when selected (fix by reloading page). Fixes in progress:</li>
-          <ol>
-            <li>Make sure web scraper picks up images for all variants (i.e. megas, etc)</li>
-            <li>Give cards a way to deal with an image not being found (i.e. '?' figure on showdown)</li>
-          </ol>
           <li>Working on adding sliders for all EV's</li>
           <ul>
             <li>Base stat display will be combined with stat sliders, similar to EV/IV mgmt on showdown teambuilder</li>
