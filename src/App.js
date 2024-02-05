b9fa52271c2fe5116fe868c40ba2026c84d5189e
Stephen Kelman <stephenrkelman@gmail.com>
2024-02-05 13:54:41 -0800

    removed completed WIP notes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,6 @@ function App() {
     <div className="App">
       <h1>Pokemon Damage Calculator Coming Soon!</h1>
       {<CardBox cards={[1,1]}/>}
-      <div style={{textAlign:"left"}}>
-        <h2>Known Issues/WIP:</h2>
-        <ul>
-          <li>Working on adding sliders for all EV's</li>
-          <ul>
-            <li>Base stat display will be combined with stat sliders, similar to EV/IV mgmt on showdown teambuilder</li>
-          </ul>
-          <li>Working on allowing text input for dropdown, to make it searchable</li>
-        </ul>
-      </div>
     </div>
   );
 }
