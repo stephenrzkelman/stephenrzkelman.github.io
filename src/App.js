bdea8d4e4d83801d1a68d0091d8e487a28232e86
Stephen Kelman <stephenrkelman@gmail.com>
2024-02-17 18:24:31 -0800

    added list of source codes to website

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ function App() {
       <h1>Pokemon Damage Calculator Coming Soon!</h1>
       {/* <CardBox cards={[1,1]}/> */}
       <Duel/>
+      <div style={{textAlign:"left"}}>
+        <p>Source Code:</p>
+        <ul>
+          <li><a href="https://github.com/stephenrzkelman/pokemondb-scraper">pokemondb.net web scraper</a></li>
+          <li><a href="https://github.com/stephenrzkelman/stephenrzkelman.github.io">code for this page</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
