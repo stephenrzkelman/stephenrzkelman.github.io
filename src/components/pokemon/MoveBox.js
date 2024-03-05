ee3604611e36852025aa3233cbb91feb89fc5756
Stephen Kelman <stephenrkelman@gmail.com>
2024-03-04 19:26:43 -0800

    added crit damage calc

--- src/components/pokemon/MoveBox.js
+++ src/components/pokemon/MoveBox.js
@@ -24,27 +24,31 @@ function MoveBox(props) {
         let moveCat = moveInfo["category"];
         let movePower = Number(moveInfo["power"]);
         let attackerStat;
-        let attackerModifiedStat;
+        let attackerCritStat;
         let defenderStat;
-        let defenderModifiedStat;
+        let defenderCritStat;
         if(moveCat === "Special"){
-            attackerStat = Number(props.attackerStats["Sp. Atk"]);
-            attackerModifiedStat = attackerStat * statModifierCalc(props.attackerStatChanges["Sp. Atk"]);
-            defenderStat = Number(props.defenderStats["Sp. Def"]);
-            defenderModifiedStat = defenderStat * statModifierCalc(props.defenderStatChanges["Sp. Def"]);
+            let attackerEntryStat = Number(props.attackerStats["Sp. Atk"]);
+            attackerStat = attackerEntryStat * statModifierCalc(props.attackerStatChanges["Sp. Atk"]);
+            attackerCritStat = Math.max(attackerEntryStat, attackerStat);
+            let defenderEntryStat = Number(props.defenderStats["Sp. Def"]);
+            defenderStat = defenderEntryStat * statModifierCalc(props.defenderStatChanges["Sp. Def"]);
+            defenderCritStat = Math.min(defenderEntryStat, defenderStat);
         }
         else if(moveCat === "Physical"){
-            attackerStat = Number(props.attackerStats["Attack"]);
-            attackerModifiedStat = attackerStat * statModifierCalc(props.attackerStatChanges["Attack"]);
-            defenderStat = Number(props.defenderStats["Defense"]);
-            defenderModifiedStat = defenderStat * statModifierCalc(props.defenderStatChanges["Defense"]);
+            let attackerEntryStat = Number(props.attackerStats["Attack"]);
+            attackerStat = attackerEntryStat * statModifierCalc(props.attackerStatChanges["Attack"]);
+            attackerCritStat = Math.max(attackerEntryStat, attackerStat);
+            let defenderEntryStat = Number(props.defenderStats["Defense"]);
+            defenderStat = defenderEntryStat * statModifierCalc(props.defenderStatChanges["Defense"]);
+            defenderCritStat = Math.min(defenderEntryStat, defenderStat);
         }
         else{
             return 0;
         }
         let levelMultiplier = Math.floor(2 * (props.level) / 5) + 2;
-        let baseMultiplier = Math.floor((levelMultiplier * movePower * attackerModifiedStat / defenderModifiedStat)/50) + 2;
-        let critMultiplier = Math.floor((levelMultiplier * movePower * attackerStat / defenderStat)/50) + 2;
+        let baseMultiplier = Math.floor((levelMultiplier * movePower * attackerStat / defenderStat)/50) + 2;
+        let baseCritMultiplier = Math.floor((levelMultiplier * movePower * attackerCritStat / defenderCritStat)/50) + 2;
 
         let sameTypeAttackBonus = props.attackerType.includes(moveType) ? 1.5 : 1;
         let typeEffectiveness = props.defenderType.reduce(
@@ -54,9 +58,10 @@ function MoveBox(props) {
 
         let highDamage = Math.floor(baseMultiplier * sameTypeAttackBonus * typeEffectiveness);
         let lowDamage = Math.floor(0.85 * highDamage);
-        let critDamage = Math.floor(baseMultiplier * sameTypeAttackBonus * typeEffectiveness * 1.5);
+        let critHighDamage = Math.floor(baseCritMultiplier * sameTypeAttackBonus * typeEffectiveness * 1.5);
+        let critLowDamage = Math.floor(0.85 * critHighDamage);
 
-        setDamage([lowDamage, highDamage, critDamage]);
+        setDamage([lowDamage, highDamage, critLowDamage, critHighDamage]);
     }
 
     useEffect(()=>damageCalc());
@@ -66,6 +71,7 @@ function MoveBox(props) {
                 display:"flex",
                 flexDirection:"column",
                 alignItems:"baseline",
+                justifyContent:"flex-start",
                 width:"100%"
             }}
         >
@@ -92,13 +98,21 @@ function MoveBox(props) {
                     items={Object.keys(allMoves)}
                     selectionAction={(selectedMove)=>setMove(selectedMove)}
                 />
-                <p
+                <table
                     style={{
-                        width:"20rem",
+                        width:"25rem",
                         textAlign:"right"
                     }}
-                >Damage: {damage[0]} - {damage[1]}
-                </p>
+                >
+                    <tr>
+                        <td>Damage:</td>
+                        <td>{damage[0]} - {damage[1]}</td>
+                    </tr>
+                    <tr>
+                        <td>Crit:</td>
+                        <td>{damage[2]} - {damage[3]}</td>
+                    </tr>
+                </table>
             </div>
         </div>
     )
