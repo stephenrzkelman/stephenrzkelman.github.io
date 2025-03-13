fc7625d2aa2b056a99eca5c9f287307b2c193d24
Stephen Kelman <stephenrkelman@gmail.com>
2025-03-12 22:22:08 -0700

    fixing weird whitespace border issue with display

--- stephenrzkelman.github.io/src/index.js
+++ stephenrzkelman.github.io/src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
